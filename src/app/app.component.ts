3b7d19bc939111b279077ee3ef1b253fb8332a4b
weirdyang <wy4786@gmail.com>
2021-06-13 22:32:29 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dark-mode';
+}
