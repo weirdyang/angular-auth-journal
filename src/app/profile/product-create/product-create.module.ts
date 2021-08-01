9cb5ade876d3d67297b22fb37b54c245c5547e33
weirdyang <wy4786@gmail.com>
2021-08-02 00:59:04 +0800

    feat(product-create): init component for product creation

---
+++ src/app/profile/product-create/product-create.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductCreateComponent } from './product-create.component';
+
+
+@NgModule({
+  declarations: [
+    ProductCreateComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProductCreateModule { }
